743acb3eaf427cafedc4fa369d8260930791cfb1
LookerKy <nagaraz07@gmail.com>
2019-06-15 15:02:45 +0900

    start config

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -1,1 +1,5 @@
-export default {};
+export default {
+  User: {
+    id: () => 123
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
-import { GraphQLServers } from "graphql-yoga";
-import schema from "./schema";
+import { GraphQLServer } from "graphql-yoga";
+// import schema from "./schema";
 import "./env";
 
 const PORT = process.env.PORT || 4000;
-
-const server = new GraphQLServers({
-  schema
-});
+const typeDefs = `
+    type Query {
+        hello(name : String) : String
+    }
+`;
+//api endpoint 구현
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => {
+      return name;
+    }
+  }
+};
+const server = new GraphQLServer({ typeDefs, resolvers });
 
 server.start({ port: PORT }, () => {
   console.log(`Server running on http://localhost:${PORT}`);
